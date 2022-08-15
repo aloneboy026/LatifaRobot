c82711188a3cc1ee065ae19e8b755b4f55b17922
ＲＯＭＥＯ <109813393+TeamLegend77@users.noreply.github.com>
2022-08-16 02:39:45 +0530

    💞

--- LatifaRobot/sample_config.py
+++ LatifaRobot/sample_config.py
@@ -19,7 +19,7 @@ class Config(object):
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 1356469075  # If you dont know, run the bot and do /id in your private chat with it, also an integer
     OWNER_USERNAME = "XH4REEF_L4DK4_43"
-    SUPPORT_CHAT = "FriendsForever_77"  # Your own group for support, do not add the @
+    SUPPORT_CHAT = "LegendSupport"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
         -1001636750801
     )  # Prints any new group the bot is added to, prints just the name and ID.
