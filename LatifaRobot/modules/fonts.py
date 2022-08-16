37704b8a532420dca0e8e4bdab7c5c05c0793717
ＲＯＭＥＯ <109813393+TeamLegend77@users.noreply.github.com>
2022-08-16 16:53:28 +0530

    💞

--- LatifaRobot/modules/fonts.py
+++ LatifaRobot/modules/fonts.py
@@ -1,6 +1,6 @@
-from FallenRobot import dispatcher
-from FallenRobot.modules.disable import DisableAbleCommandHandler
-from FallenRobot.modules.helper_funcs.alternate import typing_action
+from LatifaRobot import dispatcher
+from LatifaRobot.modules.disable import DisableAbleCommandHandler
+from LatifaRobot.modules.helper_funcs.alternate import typing_action
 from telegram import ParseMode
 from telegram.ext import run_async
 
