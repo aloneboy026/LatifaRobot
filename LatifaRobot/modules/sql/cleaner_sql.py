30bd30205a001739dfd6943d8e061d0c607509a4
ＲＯＭＥＯ <109813393+TeamLegend77@users.noreply.github.com>
2022-08-16 21:47:59 +0530

    💞

--- LatifaRobot/modules/sql/cleaner_sql.py
+++ LatifaRobot/modules/sql/cleaner_sql.py
@@ -1,7 +1,7 @@
 import threading
 
 from sqlalchemy import Boolean, Column, UnicodeText
-from FallenRobot.modules.sql import BASE, SESSION
+from LatifaRobot.modules.sql import BASE, SESSION
 
 
 class CleanerBlueTextChatSettings(BASE):
