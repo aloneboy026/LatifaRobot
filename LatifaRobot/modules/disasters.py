82128b7e91cc4bc35c7dfe212cc9008bdec34b21
ＲＯＭＥＯ <109813393+TeamLegend77@users.noreply.github.com>
2022-08-16 16:43:07 +0530

    💞

--- LatifaRobot/modules/disasters.py
+++ LatifaRobot/modules/disasters.py
@@ -3,7 +3,7 @@ import json
 import os
 from typing import Optional
 
-from FallenRobot import (
+from LatifaRobot import (
     DEV_USERS,
     OWNER_ID,
     DRAGONS,
@@ -13,13 +13,13 @@ from FallenRobot import (
     WOLVES,
     dispatcher,
 )
-from FallenRobot.modules.helper_funcs.chat_status import (
+from LatifaRobot.modules.helper_funcs.chat_status import (
     dev_plus,
     sudo_plus,
     whitelist_plus,
 )
-from FallenRobot.modules.helper_funcs.extraction import extract_user
-from FallenRobot.modules.log_channel import gloggable
+from LatifaRobot.modules.helper_funcs.extraction import extract_user
+from LatifaRobot.modules.log_channel import gloggable
 from telegram import ParseMode, TelegramError, Update
 from telegram.ext import CallbackContext, CommandHandler, run_async
 from telegram.utils.helpers import mention_html
