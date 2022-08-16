1e9aa541cfbe28683ed6aa0cd0e7af7d34863c90
ＲＯＭＥＯ <109813393+TeamLegend77@users.noreply.github.com>
2022-08-16 22:23:13 +0530

    💞

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
-TITLE Fallen Robot
-:: Enables virtual env mode and then starts Fallen
-env\scripts\activate.bat && py -m FallenRobot
+TITLE Latifa Robot
+:: Enables virtual env mode and then starts Latifa
+env\scripts\activate.bat && py -m LatifaRobot
