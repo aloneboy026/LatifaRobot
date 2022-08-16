865ed718b67d0f5baa6f57622f934bf16ea32a8e
ＲＯＭＥＯ <109813393+TeamLegend77@users.noreply.github.com>
2022-08-16 19:48:45 +0530

    💞

--- LatifaRobot/modules/locks.py
+++ LatifaRobot/modules/locks.py
@@ -9,20 +9,20 @@ from telegram.utils.helpers import mention_html
 
 from alphabet_detector import AlphabetDetector
 
-import FallenRobot.modules.sql.locks_sql as sql
-from FallenRobot import dispatcher, DRAGONS, LOGGER
-from FallenRobot.modules.disable import DisableAbleCommandHandler
-from FallenRobot.modules.helper_funcs.chat_status import (
+import LatifaRobot.modules.sql.locks_sql as sql
+from LatifaRobot import dispatcher, DRAGONS, LOGGER
+from LatifaRobot.modules.disable import DisableAbleCommandHandler
+from LatifaRobot.modules.helper_funcs.chat_status import (
     can_delete,
     is_user_admin,
     user_not_admin,
     is_bot_admin,
     user_admin,
 )
-from FallenRobot.modules.log_channel import loggable
-from FallenRobot.modules.connection import connected
-from FallenRobot.modules.sql.approve_sql import is_approved
-from FallenRobot.modules.helper_funcs.alternate import send_message, typing_action
+from LatifaRobot.modules.log_channel import loggable
+from LatifaRobot.modules.connection import connected
+from LatifaRobot.modules.sql.approve_sql import is_approved
+from LatifaRobot.modules.helper_funcs.alternate import send_message, typing_action
 
 ad = AlphabetDetector()
 
