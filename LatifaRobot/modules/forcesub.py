10d08b0edd141a9c63e37cf07ec7d78c114f310d
ＲＯＭＥＯ <109813393+TeamLegend77@users.noreply.github.com>
2022-08-16 16:55:00 +0530

    💞

--- LatifaRobot/modules/forcesub.py
+++ LatifaRobot/modules/forcesub.py
@@ -10,9 +10,9 @@ from pyrogram.errors.exceptions.bad_request_400 import (
 )
 from pyrogram.types import ChatPermissions, InlineKeyboardButton, InlineKeyboardMarkup
 
-from FallenRobot import DRAGONS as SUDO_USERS
-from FallenRobot import pbot
-from FallenRobot.modules.sql import forceSubscribe_sql as sql
+from LatifaRobot import DRAGONS as SUDO_USERS
+from LatifaRobot import pbot
+from LatifaRobot.modules.sql import forceSubscribe_sql as sql
 
 
 logging.basicConfig(level=logging.INFO)
