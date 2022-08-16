0fe7df08c8e073c881045bbe7ec955324fbedc1e
ＲＯＭＥＯ <109813393+TeamLegend77@users.noreply.github.com>
2022-08-16 12:32:47 +0530

    💞

--- LatifaRobot/modules/blacklist_stickers.py
+++ LatifaRobot/modules/blacklist_stickers.py
@@ -1,17 +1,17 @@
 import html
 from typing import Optional
 
-import FallenRobot.modules.sql.blsticker_sql as sql
-from FallenRobot import LOGGER, dispatcher
-from FallenRobot.modules.connection import connected
-from FallenRobot.modules.disable import DisableAbleCommandHandler
-from FallenRobot.modules.helper_funcs.alternate import send_message
-from FallenRobot.modules.helper_funcs.chat_status import user_admin, user_not_admin
-from FallenRobot.modules.helper_funcs.misc import split_message
-from FallenRobot.modules.helper_funcs.string_handling import extract_time
+import LatifaRobot.modules.sql.blsticker_sql as sql
+from LatifaRobot import LOGGER, dispatcher
+from LatifaRobot.modules.connection import connected
+from LatifaRobot.modules.disable import DisableAbleCommandHandler
+from LatifaRobot.modules.helper_funcs.alternate import send_message
+from LatifaRobot.modules.helper_funcs.chat_status import user_admin, user_not_admin
+from LatifaRobot.modules.helper_funcs.misc import split_message
+from LatifaRobot.modules.helper_funcs.string_handling import extract_time
 
-from FallenRobot.modules.log_channel import loggable
-from FallenRobot.modules.warns import warn
+from LatifaRobot.modules.log_channel import loggable
+from LatifaRobot.modules.warns import warn
 from telegram import Chat, Message, ParseMode, Update, User, ChatPermissions
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, Filters, MessageHandler
