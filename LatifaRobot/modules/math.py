095f906b805152dda0f55fc6d04239fd103d7adc
ＲＯＭＥＯ <109813393+TeamLegend77@users.noreply.github.com>
2022-08-16 20:20:32 +0530

    💞

--- LatifaRobot/modules/math.py
+++ LatifaRobot/modules/math.py
@@ -1,8 +1,8 @@
 import math
 
 import pynewtonmath as newton
-from FallenRobot import dispatcher
-from FallenRobot.modules.disable import DisableAbleCommandHandler
+from LatifaRobot import dispatcher
+from LatifaRobot.modules.disable import DisableAbleCommandHandler
 from telegram import Update
 from telegram.ext import CallbackContext, run_async
 
