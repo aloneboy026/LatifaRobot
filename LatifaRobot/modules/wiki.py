92a1c8ab926bcb9f30dfd1d6dfdf209f3fe9c6f1
ＲＯＭＥＯ <109813393+TeamLegend77@users.noreply.github.com>
2022-08-16 16:38:47 +0530

    💞

--- LatifaRobot/modules/wiki.py
+++ LatifaRobot/modules/wiki.py
@@ -1,6 +1,6 @@
 import wikipedia
-from FallenRobot import dispatcher
-from FallenRobot.modules.disable import DisableAbleCommandHandler
+from LatifaRobot import dispatcher
+from LatifaRobot.modules.disable import DisableAbleCommandHandler
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 from wikipedia.exceptions import DisambiguationError, PageError
