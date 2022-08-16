2645c19da8f23c17c801b98e700c0786eea0e5f2
ＲＯＭＥＯ <109813393+TeamLegend77@users.noreply.github.com>
2022-08-16 21:50:15 +0530

    💞

--- LatifaRobot/modules/sql/cust_filters_sql.py
+++ LatifaRobot/modules/sql/cust_filters_sql.py
@@ -2,8 +2,8 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, Boolean, BigInteger, distinct, func
 
-from FallenRobot.modules.helper_funcs.msg_types import Types
-from FallenRobot.modules.sql import BASE, SESSION
+from LatifaRobot.modules.helper_funcs.msg_types import Types
+from LatifaRobot.modules.sql import BASE, SESSION
 
 
 class CustomFilters(BASE):
