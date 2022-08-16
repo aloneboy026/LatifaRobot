19d1088d090fe66e22b61be897ed906b128ccdc6
ＲＯＭＥＯ <109813393+TeamLegend77@users.noreply.github.com>
2022-08-16 16:50:42 +0530

    💞

--- LatifaRobot/modules/english.py
+++ LatifaRobot/modules/english.py
@@ -1,7 +1,7 @@
-from FallenRobot import telethn as tbot
+from LatifaRobot import telethn as tbot
 import json
 import requests
-from FallenRobot.events import register
+from LatifaRobot.events import register
 from telethon import *
 from telethon.tl import functions
 import os
