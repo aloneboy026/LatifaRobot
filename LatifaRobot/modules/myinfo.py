8a95b41f611b7bffca6eefd2db90d879983d9dc8
ＲＯＭＥＯ <109813393+TeamLegend77@users.noreply.github.com>
2022-08-16 20:29:25 +0530

    💞

--- LatifaRobot/modules/myinfo.py
+++ LatifaRobot/modules/myinfo.py
@@ -9,20 +9,20 @@ from datetime import datetime
 import random
 from PIL import Image
 from io import BytesIO
-from FallenRobot import telethn as bot
-from FallenRobot import telethn as tgbot
-from FallenRobot.events import register
-from FallenRobot import dispatcher
+from LatifaRobot import telethn as bot
+from LatifaRobot import telethn as tgbot
+from LatifaRobot.events import register
+from LatifaRobot import dispatcher
 
 
 edit_time = 5
-""" =======================FALLEN ROBOT====================== """
+""" =======================LATIFA ROBOT====================== """
 file1 = "https://telegra.ph/file/9a85d0a873e2dd80d278d.jpg"
 file2 = "https://telegra.ph/file/9e7815284031452afa9e5.jpg"
 file3 = "https://telegra.ph/file/dcc5e003287f69acea368.jpg"
 file4 = "https://telegra.ph/file/ed1ce7fee94f46b0f671e.jpg"
 file5 = "https://telegra.ph/file/701028ce085ecfa961a36.jpg"
-""" =======================FALLEN ROBOT====================== """
+""" =======================LATIFA ROBOT====================== """
 
 
 @register(pattern="/myinfo")
@@ -71,7 +71,7 @@ async def callback_query_handler(event):
     try:
         boy = event.sender_id
         PRO = await bot.get_entity(boy)
-        LILIE = "POWERED BY 𝝙𝗡𝗢𝗡𝗬𝗠𝗢𝗨𝗦 \n\n"
+        LILIE = "POWERED BY ༺۞ ＲＯＭＥＯ ۞༻ \n\n"
         LILIE += f"FIRST NAME : {PRO.first_name} \n"
         LILIE += f"LAST NAME : {PRO.last_name}\n"
         LILIE += f"YOU BOT : {PRO.bot} \n"
