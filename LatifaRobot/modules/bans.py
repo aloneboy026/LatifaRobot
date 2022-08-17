0f33307e3f2a712802e060b8f41ce8103a9eced3
ＲＯＭＥＯ <109813393+TeamLegend77@users.noreply.github.com>
2022-08-17 09:04:39 +0530

    Update bans.py

--- LatifaRobot/modules/bans.py
+++ LatifaRobot/modules/bans.py
@@ -5,7 +5,7 @@ from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, Filters, run_async
 from telegram.utils.helpers import mention_html
 
-from FallenRobot import (
+from LatifaRobot import (
     DEV_USERS,
     LOGGER,
     OWNER_ID,
