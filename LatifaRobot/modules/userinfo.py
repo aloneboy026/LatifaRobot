0faaaabfdd9541b9da5d00ada3ac5c34aaf0ffce
ＲＯＭＥＯ <109813393+TeamLegend77@users.noreply.github.com>
2022-08-17 09:08:19 +0530

    Update userinfo.py

--- LatifaRobot/modules/userinfo.py
+++ LatifaRobot/modules/userinfo.py
@@ -19,7 +19,7 @@ from telegram.ext.dispatcher import run_async
 from telegram.error import BadRequest
 from telegram.utils.helpers import escape_markdown, mention_html
 
-from FallenRobot import (
+from LatifaRobot import (
     DEV_USERS,
     OWNER_ID,
     DRAGONS,
