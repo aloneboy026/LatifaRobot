33a3a0c94b32412ddb163d9fc156e3ba88ce2161
TeamLegend77 <102808014+TeamLegend77@users.noreply.github.com>
2022-10-18 06:28:09 +0000

    Code Formatting

--- LatifaRobot/__main__.py
+++ LatifaRobot/__main__.py
@@ -791,8 +791,7 @@ def main():
     settings_handler = CommandHandler("settings", get_settings)
     settings_callback_handler = CallbackQueryHandler(settings_button, pattern=r"stngs_")
 
-    info_callback_handler = CallbackQueryHandler(
-        info_about_callback, pattern=r"info_")
+    info_callback_handler = CallbackQueryHandler(info_about_callback, pattern=r"info_")
 
     music_callback_handler = CallbackQueryHandler(
         Music_about_callback, pattern=r"music_"
